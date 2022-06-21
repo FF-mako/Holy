5320d55bd651516d761aaffe63d567a60a30e0a8
Rurutiaza llty <mako6.5shealth@outlook.com>
2022-06-21 21:39:31 +0800

    no

---
+++ Algorithm/src/Leetcode/Top100/t03/Solution.java
@@ -0,0 +1,20 @@
+package Leetcode.Top100.t03;
+
+//2022-06-21
+//3.无重复字符的最长子串
+//难度：中等
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        char[] chars = s.toCharArray();
+        int len = chars.length;
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = 0; j < chars.length - i; j++) {
+
+                
+            }
+        }
+
+
+        return 0;
+    }
+}
